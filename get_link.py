65969a01d28039e767c68d4e82e2418cf1848b69
Otto Sabart <seberm@gmail.com>
2012-07-03 13:11:48 +0200

    initial commit

---
+++ get_link.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import urllib.request 
+
+
+# Ziskame stranku
+fd = urllib.request.urlopen('http://www.titulky.com/Terminator-83734.htm')
+htmlSource = str(fd.read().decode('cp1250'))
+fd.close()
+
+
+# z tohoto ziskame adresu href..., a jeji cilovou stranku stahneme do tmp...a znovu rozparsujeme
+
+
+#<a[^>]+id="bleh"[^>]+href="([^"]+)"[^>]*>
+#pattern = r'<a[\s]+class="titulkydownloadajax[\s]cboxElement"[\s]+href="(.*?)"' #funguje
+#pattern = r'<a[\s]+class="titulkydownloadajax"[\s]+href="(.*?)"'
+
+pattern = r'''
+            <a
+            [\s]+                            # Ignorujeme bile znaky
+            class="titulkydownloadajax"      # Najdeme spravny HTML prvek
+            [\s]+
+            href="(?P<addr>[^"]+)"[^>]*      # Najdeme adresu, ktera je v href="..." a pojmenujeme si ji 'addr'
+            >
+            (?P<name>[^<]*)</a>              # Ulozime si i nazev filmu / serialu, pro ktery titulky stahujeme 'name'
+           '''
+
+data = re.search(pattern, htmlSource, re.VERBOSE)
+
+if data:
+    link = 'http://www.titulky.com/' + data.group('addr')
+    name = data.group('name')
+
+    #print('Adresa iframe:', link)
+    #print('Jmeno serialu/filmu:', name)
+
+    fd = urllib.request.urlopen(link)
+    iframe = str(fd.read().decode('cp1250'))
+    fd.close()
+
+    pattern = r'<a[\s]+[^h]+href="(?P<addr>[^"]+)"[^>]*>'
+    data = re.search(pattern, iframe, re.VERBOSE)
+    resLink = 'http://www.titulky.com' + data.group('addr')
+
+    # Vytiskneme link na stdout
+    print(resLink)
+
+else: print('No data found')
+
