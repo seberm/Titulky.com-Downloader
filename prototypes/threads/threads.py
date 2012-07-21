b8d0d40306024f22733df177de1f074db2f14706
Otto Sabart <seberm@gmail.com>
2012-07-21 15:27:00 +0200

    Some tries with threads

--- prototypes/threads/threads.py
+++ prototypes/threads/threads.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python
 
 import threading
+import time
+
+titlesLinks = []
+
 
 class Foo (threading.Thread):
     def __init__(self,x):
         self.__x = x
+        self.__end = false
         threading.Thread.__init__(self)
 
     def run (self):
           print(str(self.__x), end=", ")
+          print('putting to list ')
+          #sleep(
+          global titlesLinks
+          titlesLinks.append('cislo: %d' % self.__x)
 
+threds = []
+for x in range(20):
+    threds.append(Foo(x))
 
-for x in range(50):
-    Foo(x).start()
+for t in threds:
+    t.start()
+
+print('all threads started')
+
+print(list(titlesLinks))
