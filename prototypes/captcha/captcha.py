5f6b158dd0e38f481433a8d9f4c809ab12898cb9
Otto Sabart <seberm@gmail.com>
2012-08-20 00:24:50 +0200

    Loading captcha image into label is now working; it's possible reload the image with a button

--- prototypes/captcha/captcha.py
+++ prototypes/captcha/captcha.py
@@ -1,46 +1,62 @@
-#  http://www.titulky.com/idown.php?R=1342961604&titulky=0000170819&zip=&histstamp=
-#  http://www.titulky.com/captcha/captcha.php
-
 import sys
 from PyQt4 import QtGui, QtCore, QtNetwork
 
 from PyQt4.QtCore import SLOT, SIGNAL
+
+CAPTCHA_URL = 'http://www.titulky.com/captcha/captcha.php'
+
 
 class CaptchaDialog(QtGui.QDialog):
 
     def __init__(self, parent = None, flags = 0):
         super(CaptchaDialog, self).__init__(parent)
 
-        self.layout = QtGui.QGridLayout()
+        # Dialog settings
+        self.setWindowTitle('Re-type captcha')
 
-        #layout.addWidget(btn)
-        self.lblCaptcha = QtGui.QLabel('a', self)
+        # Widgets
+        self.lblCaptcha = QtGui.QLabel('No captcha loaded', self)
+        self.lblCaptcha.setFixedSize(200, 70)
 
+        self.btnReload = QtGui.QPushButton('Reload', self)
+        self.connect(self.btnReload, SIGNAL("clicked()"), self.reloadCaptcha)
+        self.btnSend = QtGui.QPushButton('Send', self)
 
+        layout = QtGui.QGridLayout()
+        layout.addWidget(self.lblCaptcha)
+        layout.addWidget(self.btnReload)
+        layout.addWidget(self.btnSend)
 
-        manager = QtNetwork.QNetworkAccessManager(self)
-        self.connect(manager, SIGNAL("finished(reply)"), self, SLOT('managerFinished(self, reply)'))
+        self.setLayout(layout)
+        
 
-        #url = QtNetwork.QUrl('http://www.titulky.com/captcha/captcha.php')
-        url = QtCore.QUrl('http://www.titulky.com/captcha/captcha.php')
-        request = QtNetwork.QNetworkRequest(url)
+        # Load captcha into label
+        self.manager = QtNetwork.QNetworkAccessManager(self)
+        self.connect(self.manager, SIGNAL("finished(QNetworkReply*)"), self.managerFinished)
 
-        manager.get(request)
+        self.reloadCaptcha()
 
 
 
     def managerFinished(self, reply):
-        print('ahoj')
 
         if reply.error() != QtNetwork.QNetworkReply.NoError:
-            print('nastala chyba')
+            print('Error in loading captcha...')
             print(reply.errorString())
 
         data = reply.readAll()
-        pixmap = QtCore.QPixmap()
+        pixmap = QtGui.QPixmap()
         pixmap.loadFromData(data)
 
         self.lblCaptcha.setPixmap(pixmap)
+
+
+    def reloadCaptcha(self):
+
+        url = QtCore.QUrl(CAPTCHA_URL)
+        request = QtNetwork.QNetworkRequest(url)
+
+        self.manager.get(request)
 
 
 
