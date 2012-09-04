eb4ab590744b451bd4111a64fde2121af35e645a
Otto Sabart <seberm@gmail.com>
2012-09-04 16:35:29 +0200

    Added test of regular expression - when we are finding iframe links

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -22,39 +22,55 @@ os.chdir(WORK_DIR)
 class ManagerTests(unittest.TestCase):
 
     def setUp(self):
-
-        self.pageUrl = urllib.parse.urlparse('file://' + TEST_FILES).geturl()
-        self.manager = Manager.Manager(encoding='cp1250', page=self.pageUrl)
+        self._pageUrl = urllib.parse.urlparse('file://' + TEST_FILES).geturl()
+        self._manager = Manager.Manager(encoding='cp1250', page=self._pageUrl)
 
 
     def test_managerInit(self):
+        '''Testing manager object inicialization'''
         self.assertTrue(os.path.isdir(TEST_FILES))
-        self.assertEqual(self.manager.encoding, 'cp1250')
-        self.assertEqual(self.manager.page, self.pageUrl)
+        self.assertEqual(self._manager._encoding, 'cp1250')
+        self.assertEqual(self._manager._page, self._pageUrl)
 
+
+    def test_getIframeLinks(self):
+        '''We are testing regular expression for finding iframe links'''
+        url = urllib.parse.urlparse('file://' + TEST_FILES + '/Basic_Page-test-iframe-links.htm')
+        iframeLinks = self._manager.getIframeLinks(url=url.geturl(), encoding='cp1250')
+        
+        self.assertIn(('http://titulky.com/', 'Titulky.com'), iframeLinks)
+        self.assertIn(('/idown.php?R=1341223560&amp;titulky=0000000615&amp;zip=&amp;histstamp=', 'Link 1'), iframeLinks)
+        self.assertIn(('http://www.titulky.com/idown.php?R=1341223560&amp;titulky=0000000615&amp;zip=&amp;histstamp=', 'Link 2'), iframeLinks)
+        self.assertIn(('idown.php?R=1341223560&amp;titulky=0000000615&amp;zip=&amp;histstamp=', 'Link 3'), iframeLinks)
+
+        # There shouldn't be other links
+        self.assertIs(len(iframeLinks), 4)
+
+                
 
     def test_getLinks(self):
         url = urllib.parse.urlparse('file://' + TEST_FILES + '/Some_Movie-170819.htm')
-        self.manager.getLinks(url=url.geturl())
+        self._manager.getSubtitleSourceLinks(url=url.geturl())
 
-        self.assertIn({'url' : self.pageUrl + 'idown.php?id=90801970',
+        self.assertIn({'url' : self._pageUrl + 'idown.php?id=90801970',
                        'name' : 'Some Movie (CD 1)',
                        'wait' : datetime.now().hour,
-                      }, self.manager.links)
+                      }, self._manager._links)
 
-        self.assertIn({'url' : self.pageUrl + 'idown.php?id=90801970',
+        self.assertIn({'url' : self._pageUrl + 'idown.php?id=90801970',
                        'name' : 'Some Movie (CD 2)',
                        'wait' : datetime.now().hour,
-                      }, self.manager.links)
+                      }, self._manager._links)
 
-        self.assertIs(len(self.manager.links), 2)
+        self.assertIs(len(self._manager._links), 2)
 
 
     def test_downloadFiles(self):
+        '''Test for downloading subtitle file'''
         url = urllib.parse.urlparse('file://' + TEST_FILES + '/idown.php?id=90801970')
         files = [{'name' : 'file1-downloaded', 'url' : url.geturl(), 'wait' : 0}]
 
-        self.manager.downloadFiles(links=files)
+        self._manager.downloadFiles(links=files)
         self.assertTrue(os.path.isfile(os.path.join(WORK_DIR) + '/file1-downloaded.srt'))
 
 if __name__ == '__main__':
