9de607a9f43c32da555fd1aef2cde86b071cb0c0
Otto Sabart <seberm@gmail.com>
2012-09-04 16:03:57 +0200

    Changed self._name to self._movieName because of conflict of variables

--- src/IFrameParser.py
+++ src/IFrameParser.py
@@ -11,13 +11,13 @@ from TitulkyDownloader import PAGE
 
 titlesLinks = []
 
-class IFrameParser(object, Thread):
+class IFrameParser(Thread):
 
     def __init__(self, opener, url, name, encoding, lock, page=PAGE):
         self._opener = opener
         self._url = url
         self._page = page
-        self._name = name
+        self._movieName = name
         self._encoding = encoding
         self._lock = lock
 
@@ -26,15 +26,15 @@ class IFrameParser(object, Thread):
 
     def run(self):
         global titlesLinks
-        debug('[%s]: Running new IFrameParser thread (%s)' % (self._name, self._url))
+        debug('[%s]: Running new IFrameParser thread (%s)' % (self._movieName, self._url))
          
         try:
             with self._opener.open(self._url) as fd:
                 iframe = str(fd.read().decode(self._encoding))
         except urllib.error.URLError as e:
-            error('[%s]: URL error: %s' % (self._name, e.reason))
+            error('[%s]: URL error: %s' % (self._movieName, e.reason))
         except IOError:
-            error('[%s]: IO Error - thread exiting' % self._name)
+            error('[%s]: IO Error - thread exiting' % self._movieName)
             sys.exit(1)
 
         pattern = r'''
@@ -46,20 +46,20 @@ class IFrameParser(object, Thread):
                     >                           # Tag end
                    '''
 
-        debug('[%s]: Parsing iframe ...' % self._name)
+        debug('[%s]: Parsing iframe ...' % self._movieName)
 
         data = re.search(pattern, iframe, re.VERBOSE)
 
         if data:
-            debug('[%s]: Found link: %s' % (self._name, self._page + data.group('addr')))
+            debug('[%s]: Found link: %s' % (self._movieName, self._page + data.group('addr')))
             self._lock.acquire()
-            titlesLinks.append({'name' : self._name, 'url' : self._page + data.group('addr'), 'wait' : datetime.now().hour})
+            titlesLinks.append({'name' : self._movieName, 'url' : self._page + data.group('addr'), 'wait' : datetime.now().hour})
             self._lock.release()
         else:
-            debug('[%s]: No links found' % self._name)
+            debug('[%s]: No links found' % self._movieName)
             pattern = r'<img[\s]+src="./captcha/captcha.php"[\s]+/>'
             if re.search(pattern, iframe):
-                warning('[%s]: You exhausted your free daily limit of downloads - it\'s necessary to re-type captcha code' % self._name)
+                warning('[%s]: You exhausted your free daily limit of downloads - it\'s necessary to re-type captcha code' % self._movieName)
             else:
-                info('[%s]: Cannot find data on page' % self._name)
+                info('[%s]: Cannot find data on page' % self._movieName)
 
